3a6a644a19f2ab7aeb97da51267abc4828044348
gabriel <gabrielvalino25@hotmail.com>
2024-07-04 17:50:27 -0300

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'BielBlog',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+  author: 'Gabriel Valino',
+  links: {
+    linkedin: 'https://www.linkedin.com/in/gabriel-sanches-891aba296/',
+    github: 'https://github.com/gabriel-valino',
+    twitter: 'https://x.com/BielValino',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
