a1d62800122d15471d9519aae8e4428c25801db1
gabriel <gabrielvalino25@hotmail.com>
2024-07-05 20:24:47 -0300

    Stage 6: Blog page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { Post } from '#site/content'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+
+  return format(date, "d 'de' MMMM 'de' yyyy", { locale: ptBR })
+}
+
+export function sortPosts(posts: Array<Post>) {
+  return posts.sort((a, b) => {
+    if (a.date > b.date) return -1
+    if (a.date < b.date) return 1
+    return 0
+  })
 }
