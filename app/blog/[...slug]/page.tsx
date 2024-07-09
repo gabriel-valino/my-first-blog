3f89290f7ed1f20c1313f3363debe3fb0afbbade
gabriel <gabrielvalino25@hotmail.com>
2024-07-09 20:06:07 -0300

    Stage 19: Add tags to post items

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from 'next/navigation'
 import '@/styles/mdx.css'
 import { Metadata } from 'next'
 import { siteConfig } from '@/config/site'
+import { Tag } from '@/components/tag'
 
 interface PostPageProps {
   params: {
@@ -73,7 +74,10 @@ export default async function PostPage({ params }: PostPageProps) {
 
   return (
     <article className="max-x-3xl container prose mx-auto py-6 dark:prose-invert">
-      <h1 className="mb-2">{post.title}</h1>
+      <h1 className="my-2">{post.title}</h1>
+      <div className="mb-4 flex gap-2">
+        {post.tags?.map((tag) => <Tag tag={tag} key={tag} />)}
+      </div>
       {post?.description ? (
         <p className="mt-0 text-xl text-muted-foreground">{post.description}</p>
       ) : null}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -41,7 +41,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
       {displayPosts?.length > 0 ? (
         <ul className="flex flex-col gap-4">
           {displayPosts.map((post) => {
-            const { slug, title, description, date } = post
+            const { slug, title, description, date, tags } = post
 
             return (
               <li key={slug}>
@@ -50,6 +50,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
                   title={title}
                   date={date}
                   description={description}
+                  tags={tags}
                 />
               </li>
             )
