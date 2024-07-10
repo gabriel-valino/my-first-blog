c1a0943615ac57f32cd457208f7dbe03a8626f04
gabriel <gabrielvalino25@hotmail.com>
2024-07-10 17:43:40 -0300

    Stage 20: Add tags pages

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,7 @@ import { twMerge } from 'tailwind-merge'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { Post } from '#site/content'
+import { slug } from 'github-slugger'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -35,4 +36,13 @@ export function getAllTags(posts: Array<Post>) {
 
 export function sortTagsByCount(tags: Record<string, number>) {
   return Object.keys(tags).sort((a, b) => tags[b] - tags[a])
+}
+
+export function getPostsByTagSlug(posts: Array<Post>, tag: string) {
+  return posts.filter((post) => {
+    if (!post.tags) return false
+
+    const slugifiedTags = post.tags.map((tag) => slug(tag))
+    return slugifiedTags.includes(tag)
+  })
 }
